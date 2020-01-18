73dc1e4139e57280d2266684fc8a296a685d4795
Myron William Echenim <myron.echenim@live.com>
2020-01-17 17:41:59 -0800

    implement a boolean function to check if bit is set

--- utils/setter.go
+++ utils/setter.go
@@ -5,3 +5,9 @@ func SetBit(d int, position uint) int {
 	mask := 1 << position
 	return d | mask
 }
+
+//IsBitSet function check if the bit value is set and if true return the appropriate value
+func IsBitSet(d int, position uint) bool {
+	d >>= position
+	return (d & 1) != 0
+}
